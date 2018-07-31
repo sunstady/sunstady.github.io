2ba7fb735d7c3a91eadd6bf1b3001589d0d4b6bb
Sanstyle <sunstady@gmail.com>
2018-07-31 18:22:49 +0800

    站点更新:2018-07-31 18/22/37

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -150,8 +150,8 @@ $(document).ready(function () {
 
 $(document).ready(function(){
     $(document).on('click', '.fold_hider', function(){
-        $('>.fold', this.parentNode).slideToggle(),
-        $('>:first', this).toggleClass('open'),
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
     });
     $("div.fold").css("display","none");
 });
